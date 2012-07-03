d889874a6d79adf642eb4b03d2f96806ad5c4e8a
scizzr <admin@scizzr.com>
2012-07-02 22:08:22 -0400

    Fixed error submissions. (GET string in URL was too long; using POST now)

--- ScizzrWarp/src/com/scizzr/bukkit/plugins/scizzrwarp/threads/Errors.java
+++ ScizzrWarp/src/com/scizzr/bukkit/plugins/scizzrwarp/threads/Errors.java
@@ -2,6 +2,7 @@ package com.scizzr.bukkit.plugins.scizzrwarp.threads;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
@@ -30,27 +31,34 @@ public class Errors implements Runnable {
             ver = Bukkit.getBukkitVersion();
             
             URL url = new URL(
-                //$uuid && $system && $plugname && $plugver && $ver && $err
-                String.format("http://www.scizzr.com/plugins/errors.php?") +
-                String.format("uniqid=" + "%s", URLEncoder.encode(Config.genUniqID, "UTF-8")) + 
-                String.format("&system=" + "%s", URLEncoder.encode(Main.osN, "UTF-8")) +
-                String.format("&plugname=" + "%s", URLEncoder.encode(Main.info.getName(), "UTF-8")) +
-                String.format("&plugver=" + "%s", URLEncoder.encode(Main.info.getVersion(), "UTF-8")) +
-                String.format("&ver=" + "%s", URLEncoder.encode(ver, "UTF-8")) +
-                String.format("&err=" + "%s", URLEncoder.encode(err, "UTF-8"))
-            );
+                    //$uuid && $system && $plugname && $plugver && $ver && $err
+                    String.format("http://www.scizzr.com/plugins/errors.php")
+                );
             
-            Main.log.info(url.toString());
+            String data = new String(
+                    //$uuid && $system && $plugname && $plugver && $ver && $err
+                    String.format("uniqid=" + "%s", URLEncoder.encode(Config.genUniqID, "UTF-8")) + 
+                    String.format("&system=" + "%s", URLEncoder.encode(Main.osN, "UTF-8")) +
+                    String.format("&plugname=" + "%s", URLEncoder.encode(Main.info.getName(), "UTF-8")) +
+                    String.format("&plugver=" + "%s", URLEncoder.encode(Main.info.getVersion(), "UTF-8")) +
+                    String.format("&ver=" + "%s", URLEncoder.encode(ver, "UTF-8")) +
+                    String.format("&err=" + "%s", URLEncoder.encode(err, "UTF-8"))
+                );
             
-            URLConnection conn = url.openConnection();
+            URLConnection conn = url.openConnection(); conn.setDoOutput(true);
+            
+            OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream()); writer.write(data); writer.flush();
             InputStreamReader stream = new InputStreamReader(conn.getInputStream());
+            
             BufferedReader buff = new BufferedReader(stream);
+            
             String line = buff.readLine();
             
             while (line != null) {
                 // do nothing; just post stats
                 line = buff.readLine();
             }
+            
             stream.close();
         } catch (Exception ex) {
             Main.log.info(Main.prefixConsole + "Stack trace submission failed.");
